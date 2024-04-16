18be719a72d77c629a70717710484e337011c941
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-16 09:23:28 -0700

    Updated build script

--- build.sh
+++ build.sh
@@ -12,14 +12,23 @@ export PM_CGLINUX_PATH="$PROJECT_ROOT_DIR/externals/cg-linux"
 export PM_GLEWLINUX_PATH="$PROJECT_ROOT_DIR/externals/glew-linux"
 export PM_PATHS="$PM_opengllinux_PATH;$PM_TARGA_PATH;$PM_CGLINUX_PATH;$PM_GLEWLINUX_PATH"
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get install -y --no-install-recommends llvm clang build-essential
-
-cd "$PHYSX_ROOT_DIR" || true
-python ./buildtools/cmake_generate_projects.py "$@"
-status=$?
-if [ "$status" -ne "0" ]; then
-    echo "Error $status"
-    exit 1
+if ! command -v clang &>/dev/null && ! command -v gcc &>/dev/null; then
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+    sudo apt-get install -y --no-install-recommends llvm clang build-essential
 fi
+
+(
+    cd "$PHYSX_ROOT_DIR" || true
+    rm -rf "$PROJECT_ROOT_DIR/physx/compiler/linux-*/"
+    python "$PHYSX_ROOT_DIR/buildtools/cmake_generate_projects.py" "$@"
+    status=$?
+    if [ "$status" -ne "0" ]; then
+        echo "Error $status"
+        exit 1
+    fi
+
+    cd "$PROJECT_ROOT_DIR/physx/compiler/linux-release" || true
+    # cmake --build "$PROJECT_ROOT_DIR/physx/compiler/linux-release" --config Release
+    make -j"$(nproc)"
+)
