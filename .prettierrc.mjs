8471a10058c6e0e264606ee8497d7a3f425bb9c2
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-16 11:33:05 -0700

    Reformat files with new prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/**
+ * PhysX - Prettier Configuration
+ */
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
