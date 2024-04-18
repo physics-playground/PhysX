13ae1b746fc72993945ed1d484d6a8e57a410e2c
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-18 09:53:08 -0700

    Config updates from reformatting

--- pxshared/include/foundation/PxPreprocessor.h
+++ pxshared/include/foundation/PxPreprocessor.h
@@ -503,7 +503,7 @@ PX_CUDA_CALLABLE PX_INLINE void PX_UNUSED(T const&)
 #endif
 // clang (as of version 3.9) cannot align doubles on 8 byte boundary  when compiling for Intel 32 bit target
 #if !PX_APPLE_FAMILY && !PX_EMSCRIPTEN && !(PX_CLANG && PX_X86)
-	PX_COMPILE_TIME_ASSERT(PX_OFFSET_OF(PxPackValidation, a) == 8);
+    PX_COMPILE_TIME_ASSERT(PX_OFFSET_OF(PxPackValidation, a) == 8);
 #endif
 
 // use in a cpp file to suppress LNK4221
